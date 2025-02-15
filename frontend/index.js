f83fab9cc2ad3f4603ad04302db18fb1d14c4681
Ramo0o <omar.moelhawary@gmail.com>
2025-02-15 14:16:35 +0200

    added the parsePage function and improved the display of the ayahts number

--- frontend/index.js
+++ frontend/index.js
@@ -13,12 +13,28 @@ function main(){
 function getRandomPage(){
     const pageno = Math.floor(Math.random() * 604)
     fetch(`https://api.alquran.cloud/v1/page/${pageno}/quran-uthmani`)
-    .then(response => {const data = response.json()
-        return data
+    .then(response => {const quranData = response.json()
+        return quranData
     })
-    .then(data =>{
-        const page = data.data.ayahs
-        document.getElementById("quran-verse").innerHTML = page
+    .then(quranData =>{
+        const page = quranData.data.ayahs
+        parsePage(page)
     })
+}
+
+
+function parsePage(ayahs){
+    let element = ""
+    let lastSurahNo = 0
+    for (i of ayahs){
+        if(i["surah"]["number"] !== lastSurahNo){
+            lastSurahNo = i["surah"]["number"];
+            element += "<br>" + i["surah"]["name"] ;
+            element +="<br>" ;
+        }
+        element += " " +  i["text"]+ " ";
+        element += `﴿${i["numberInSurah"]}﴾`;
+    }
+    document.getElementById("quran-verse").innerHTML = element.trimStart()
 }
 main()
