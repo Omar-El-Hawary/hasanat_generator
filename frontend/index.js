c86fc8513f3fb5993e6b8cc0515558a7c419defd
Ramo0o <omar.moelhawary@gmail.com>
2025-02-15 11:44:43 +0200

    fixed the structure a bit

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,22 @@
-const pageno = Math.floor(Math.random() * 604)
-fetch(`https://api.alquran.cloud/v1/page/${pageno}/quran-uthmani`)
+function main(){
+    const parameter = window.location.search
+    if (parameter === "daily"){
+        getRandomPage()
+    }
+    // else if (hi){
+
+    // }
+}
+
+function getRandomPage(){
+    const pageno = Math.floor(Math.random() * 604)
+    fetch(`https://api.alquran.cloud/v1/page/${pageno}/quran-uthmani`)
     .then(response => {const data = response.json()
         return data
     })
     .then(data =>{
         const page = data.data.ayahs
-        .map(ayah => `${ayah.text} (${ayah.numberInSurah})`)
-        .join(' ');;
         document.getElementById("quran-verse").innerHTML = page
     })
+}
+main()
